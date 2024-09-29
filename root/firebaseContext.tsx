77039dbf301468491bb3a6ea2a3583586c91e4aa
Raghav M Daga <62994862+Raghav-M-Daga@users.noreply.github.com>
2024-09-29 15:54:15 -0700

    Update firebaseContext.tsx

--- root/firebaseContext.tsx
+++ root/firebaseContext.tsx
@@ -3,16 +3,6 @@ import { initializeApp, FirebaseApp } from 'firebase/app';
 import { initializeFirestore, Firestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBwwiZB8KX4Pt5PBeh70KPmx3cGTzgxpbk",
-    authDomain: "internfinder2.firebaseapp.com",
-    projectId: "internfinder2",
-    storageBucket: "internfinder2.appspot.com",
-    messagingSenderId: "23906885148",
-    appId: "1:23906885148:web:f6ce50a5a39fcca7ccc9d9",
-    measurementId: "G-9ESDEJJ617"
-  };
-
 const app: FirebaseApp = initializeApp(firebaseConfig);
 const db: Firestore = initializeFirestore(app, {
   experimentalForceLongPolling: true,
